24c5aed75021001f404ed54cd2c87920bbfcf692
zhengweihong <529729644@qq.com>
2021-04-26 12:11:16 +0800

    HelloWorld

---
+++ JavaBasicGrammar/src/main/java/P01_BasicGrammar/HelloWorld.java
@@ -0,0 +1,11 @@
+package P01_BasicGrammar;
+
+/**
+ * @author : ZWH 2021/4/26
+ * @version : 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
