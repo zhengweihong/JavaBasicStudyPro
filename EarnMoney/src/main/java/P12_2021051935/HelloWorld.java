a7a99f11514f41dcece4f92aef398f4683732ff1
zhengweihong <529729644@qq.com>
2021-05-20 00:12:05 +0800

    basic java taught

---
+++ EarnMoney/src/main/java/P12_2021051935/HelloWorld.java
@@ -0,0 +1,16 @@
+package P12_2021051935;
+
+import java.util.Scanner;
+
+public class HelloWorld{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a*1.0 / b*1.0);
+
+        int x=3;
+        int y=17;
+        System.out.println(-x*y+y);
+    }
+}
