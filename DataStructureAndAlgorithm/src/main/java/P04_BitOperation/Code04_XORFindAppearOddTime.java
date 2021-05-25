41015f04f98e4263d8381765b11360cf1e4e1a18
zhengweihong <529729644@qq.com>
2021-05-25 16:11:09 +0800

    Bit Operation

--- DataStructureAndAlgorithm/src/main/java/P04_BitOperation/Code04_XORFindAppearOddTime.java
+++ DataStructureAndAlgorithm/src/main/java/P04_BitOperation/Code04_XORFindAppearOddTime.java
@@ -5,11 +5,15 @@ package P04_BitOperation;
  * @version : 1.0
  */
 public class Code04_XORFindAppearOddTime {
-    public void XORFindAppearOddTime (int[] arr) {
+    public void XORFindAppearOddTime1 (int[] arr) {
         int oddNum = 0;
         for (int i = 0; i < arr.length ; i++) {
             oddNum ^= arr[i];
         }
         System.out.println(oddNum);
+    }
+
+    public void XORFindAppearOddTime2 (int[] arr) {
+
     }
 }
