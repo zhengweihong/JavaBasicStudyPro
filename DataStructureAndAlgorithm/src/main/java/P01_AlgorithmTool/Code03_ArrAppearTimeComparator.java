72cb41cc999fdcb980d99b5eb693726fb061b38f
zhengweihong <529729644@qq.com>
2021-05-26 00:15:24 +0800

    Bit Operation

---
+++ DataStructureAndAlgorithm/src/main/java/P01_AlgorithmTool/Code03_ArrAppearTimeComparator.java
@@ -0,0 +1,72 @@
+package P01_AlgorithmTool;
+
+import P04_BitOperation.Code05_XORFindAppearKTime;
+
+import java.util.HashSet;
+
+/**
+ * @author : ZWH 2021/5/25
+ * @version : 1.0
+ */
+public class Code03_ArrAppearTimeComparator {
+    public static void main(String[] args) {
+        int testTime = 50000;
+
+        int arrMaxKinds = 20;
+        int arrMaxValue = 100;
+        int appearMaxTime = 9;
+        for (int i = 0; i < testTime; i++) {
+            int a = (int)(Math.random() * appearMaxTime) + 1;//a 1-9
+            int b = (int)(Math.random() * appearMaxTime) + 1;//b 1-9
+            int k = Math.min(a,b);
+            int m = Math.max(a,b);
+            if (k == m) m++;//k < m
+
+            int[] arr = generateRandomArray(arrMaxKinds,arrMaxValue,k,m);
+            int answer1 = new Code05_XORFindAppearKTime().hashMapBuildFrequency(arr,k,m);
+            int answer2 = new Code05_XORFindAppearKTime().XORFindAppearKTime(arr,k,m);
+            if (answer1 != answer2)
+                System.out.println("Error!!!");
+        }
+        System.out.println("Finished...");
+    }
+
+    public static int[] generateRandomArray(int arrMaxKinds, int arrMaxValue, int k, int m) {
+        int trueK = Math.random() < 0.5 ? k : (int)((Math.random()*(m-1)) + 1);//实际可能出现的不是K次，1～m-1
+
+        int arrKinds = (int)(Math.random() * arrMaxKinds) + 2;//至少2种
+        int[] arr = new int[trueK + (arrKinds - 1) * m];
+
+        //range : [-maxV,+maxV]=[0,maxV]-[0,maxV]=[0,maxV]+[-maxV,0]=[-maxV,maxV]
+        int kTimeNum = (int)(Math.random() * (arrMaxValue + 1)) - (int)(Math.random() * (arrMaxValue + 1));
+
+        int index = 0;
+        for (; index < trueK ; index++) {
+            arr[index] = kTimeNum;
+        }
+        arrKinds--;//还差几种数没放arr
+
+        HashSet<Integer> set = new HashSet<>();
+        set.add(kTimeNum);
+        for (int i = 0; i < arrKinds; i++) {
+            int curNum = 0;
+            do {
+                curNum = (int)(Math.random() * (arrMaxValue + 1)) - (int)(Math.random() * (arrMaxValue + 1));
+            } while (set.contains(curNum));//包含就换一个
+            set.add(curNum);
+            for (int j = 0; j < m; j++) {
+                arr[index++] = curNum;
+            }
+        }
+
+        //随机打乱arr，i位置和随机的位置交换
+        for (int i = 0; i < arr.length/2; i++) {
+            int j = (int)(Math.random() * arr.length);//0 ~ N-1
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        return arr;
+    }
+}
---
+++ DataStructureAndAlgorithm/src/main/java/P04_BitOperation/Code05_XORFindAppearKTime.java
@@ -0,0 +1,70 @@
+package P04_BitOperation;
+
+import java.util.HashMap;
+
+/**
+ * @author : ZWH 2021/5/25
+ * @version : 1.0
+ */
+public class Code05_XORFindAppearKTime {
+    public int XORFindAppearKTime (int[] arr,int k,int m) {
+        int[] t = new int[32];//0位～31位
+        for (int arri : arr) {
+            for (int i = 0; i < 32 ; i++) {
+                //t[i] += ((arri>>i) & 1);
+                if (((arri>>i) & 1) != 0) {
+                    t[i]++;
+                }
+            }
+        }
+
+        int num = 0;
+        for (int i = 0; i < 32 ; i++) {
+            if (t[i] % m == 0)
+                continue;
+            if (t[i] % m == k) {//出现k次的数，第i位有1
+                num |= (1 << i);
+            } else {
+                return -1;//出现的次数不是k，有问题！
+            }
+            //出现k次的数，第i位有1
+            //if (t[i] % m != 0) {num |= (1 << i);}
+        }
+
+        //当出现K次的是0时，此时num是0，需要判断0是否真的出现了K次
+        if (num == 0) {
+            int count = 0;
+            for (int arri : arr) {
+                if (arri == 0)
+                    count++;
+            }
+            if (count != k)
+                return -1;
+        }
+
+        return num;
+    }
+
+    public int hashMapBuildFrequency (int[] arr,int k,int m) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int arri : arr) {
+            if (map.containsKey(arri)) {
+                map.put(arri,map.get(arri) + 1);
+            } else {
+                map.put(arri,1);
+            }
+        }
+        for (int num : map.keySet()) {
+            if (map.get(num) == k) {
+                return num;
+            }
+        }
+        return -1;//wrong answer 有问题
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0,0,2,2,2,3,3,3,4,4,4};
+        int a = new Code05_XORFindAppearKTime().XORFindAppearKTime(arr,2,3);
+        System.out.println(a);
+    }
+}
