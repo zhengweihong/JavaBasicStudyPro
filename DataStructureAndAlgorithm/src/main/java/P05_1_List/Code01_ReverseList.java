f7f98bc9543792b32c1b16c45dfb52de975cd9be
zhengweihong <529729644@qq.com>
2021-05-26 15:39:33 +0800

    reverse list

---
+++ DataStructureAndAlgorithm/src/main/java/P05_1_List/Code01_ReverseList.java
@@ -0,0 +1,40 @@
+package P05_1_List;
+
+/**
+ * @author : ZWH 2021/5/26
+ * @version : 1.0
+ */
+public class Code01_ReverseList {
+    /*
+                  head
+                   a  ->  b  ->  c  ->  null
+         null  <-  a  <-  b  <-  c
+            pre    cur
+              2<-       1next
+                  1pre   2cur
+     */
+    public MyNode reverseList (MyNode head) {
+        MyNode pre = null;
+        MyNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+    public MyDoubleNode reverseDoubleList (MyDoubleNode head) {
+        MyDoubleNode pre = null;
+        MyDoubleNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            head.pre = next;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
