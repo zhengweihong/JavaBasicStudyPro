f2aa6cddc3b7afcb9eb0c9bafa85f5c5f20d26c8
zhengweihong <529729644@qq.com>
2021-05-27 12:44:35 +0800

    Recursion getMax

---
+++ DataStructureAndAlgorithm/src/main/java/P06_Recursion/Code01_getMax.java
@@ -0,0 +1,21 @@
+package P06_Recursion;
+
+/**
+ * @author : ZWH 2021/5/27
+ * @version : 1.0
+ */
+public class Code01_getMax {
+    public int getMax(int[] arr) {
+        return getMaxRange(arr,0,arr.length-1);
+    }
+
+    public int getMaxRange(int[] arr,int L,int R) {
+        if (L == R)
+            return arr[L];
+
+        int mid = L + ((R - L) >> 1);
+        int leftMax = getMaxRange(arr,L,mid);
+        int rightMax = getMaxRange(arr,mid+1,R);
+        return Math.max(leftMax,rightMax);
+    }
+}
