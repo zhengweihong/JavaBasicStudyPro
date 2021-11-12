4e0256b9dc08a78fc0565bd7b241eb0d5f108089
zhengweihong <zhengweihong02@meituan.com>
2021-11-12 16:36:38 +0800

    添加Java基础语法模块

---
+++ JavaBasicGrammar/src/main/java/P02_CollectionFramework/C03_DelayQueueDemo/User.java
@@ -0,0 +1,40 @@
+package P02_CollectionFramework.C03_DelayQueueDemo;
+
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author : ZWH 6/3/21
+ * @version : 1.0
+ */
+public class User implements Delayed {
+    private int id;
+    private String name;
+    private long endTime;
+
+    public User(int id, String name, long endTime) {
+        this.id = id;
+        this.name = name;
+        this.endTime = endTime;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", endTime=" + endTime +
+                '}';
+    }
+
+    @Override
+    public long getDelay(TimeUnit unit) {//take
+        return endTime - System.currentTimeMillis();
+    }
+
+    @Override
+    public int compareTo(Delayed o) {//put
+        //compare endTime
+        return Long.compare(endTime, ((User) o).endTime);
+    }
+}
