7fe36af7ef29331a7b906fd66a82e4b65202939d
zhengweihong <529729644@qq.com>
2023-09-23 17:16:09 +0800

    算法 插入排序

--- DataStructureAndAlgorithm/src/main/java/P01_Sort/Code03_InsertionSort.java
+++ DataStructureAndAlgorithm/src/main/java/P01_Sort/Code03_InsertionSort.java
@@ -1,17 +1,22 @@
 package P01_Sort;
 
 /**
- * @author : ZWH 2021/5/17
- * @version : 1.0
- * Sort : from small to big
+ * @author : ZWH
+ * @date : 2021/05/17
+ * @Description : sort from small to big 插入排序
  */
 public class Code03_InsertionSort {
-    public void insertionSort(int[] arr) {
-        if (arr == null || arr.length < 2) return;
 
-        //0~0；0~1、0~i、0~N-1内部有序，i要排的数开始的位置，然后此数向左滑动
+    public void insertionSort(int[] arr) {
+        // 判空 0或1个元素
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        // 目的0~0；0~1、0~i、0~N-1有序 i是要排的数开始的位置，然后此数向左滑动
         for (int i = 1; i < arr.length; i++) {
-            //stop=at 0/left small
+            // stop=at 0/left smaller
+            // 从右往左比较 左边数大就可以交换
             for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
                 swap(arr, j, j + 1);
             }
@@ -23,4 +28,5 @@ public class Code03_InsertionSort {
         arr[i] = arr[j];
         arr[j] = tmp;
     }
+
 }
