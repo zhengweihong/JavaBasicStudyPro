63ad3cb321985c8e104ac0a5f4667e2a75d6b193
zhengweihong <529729644@qq.com>
2023-09-23 14:40:20 +0800

    算法 选择排序

--- DataStructureAndAlgorithm/src/main/java/P01_Sort/Code01_SelectionSort.java
+++ DataStructureAndAlgorithm/src/main/java/P01_Sort/Code01_SelectionSort.java
@@ -1,25 +1,31 @@
 package P01_Sort;
 
 /**
- * @author : ZWH 2021/5/17
- * @version : 1.0
- * Sort : from small to big
+ * @author : ZWH
+ * @date : 2021/05/17
+ * @Description : sort from small to big 选择排序
  */
 public class Code01_SelectionSort {
+
     public void selectionSort(int[] arr) {
-        if (arr == null || arr.length < 2) {//0\1
+        // 判空 0或1个元素
+        if (arr == null || arr.length < 2) {
             return;
         }
 
-        int N = arr.length;
-        //0~n-1、1~n-1、2~n-1...find min
-        for (int i = 0; i < N; i++) {
-            int minValueIndex = i;//arr[i]是min
-            for (int j = i + 1; j < N; j++) {
-                //minValueIndex = arr[j] < arr[minValueIndex] ? j : minValueIndex;
-                if (arr[minValueIndex] > arr[j])
+        // 0~n-1、1~n-1、2~n-1... find min
+        for (int i = 0; i < arr.length; i++) {
+            // arr[i]是min 以第一个下标为锚点
+            int minValueIndex = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                // 谁小成为新的锚点
+                // minValueIndex = arr[j] < arr[minValueIndex] ? j : minValueIndex;
+                if (arr[j] < arr[minValueIndex]) {
                     minValueIndex = j;
+                }
             }
+
             swap(arr, i, minValueIndex);
         }
     }
@@ -29,4 +35,5 @@ public class Code01_SelectionSort {
         arr[i] = arr[j];
         arr[j] = tmp;
     }
+
 }
