254901ad845187ad32a065082b2882d1c9c14e58
zhengweihong <529729644@qq.com>
2023-09-24 14:47:09 +0800

    算法 二分法

--- DataStructureAndAlgorithm/src/main/java/P03_BinarySearch/Code01_BinarySearchExist.java
+++ DataStructureAndAlgorithm/src/main/java/P03_BinarySearch/Code01_BinarySearchExist.java
@@ -1,23 +1,32 @@
 package P03_BinarySearch;
 
 /**
- * @author : ZWH 2021/5/17
- * @version : 1.0
+ * @author : ZWH
+ * @date : 2021/05/17
+ * @Description : 二分法：有序数组，找某个数是否存在
  */
 public class Code01_BinarySearchExist {
-    public boolean binarySearchExist(int[] sortedArr, int num) {
-        if (sortedArr == null || sortedArr.length == 0) return false;
+
+    public static boolean binarySearchExist(int[] sortedArr, int num) {
+        if (sortedArr == null || sortedArr.length == 0) {
+            return false;
+        }
 
         int L = 0;
         int R = sortedArr.length - 1;
         int mid;
-        //L...mid...R
-        //L..R
-        //       L..R
+
+        // 停止条件：L～R至少1个数
         while (L <= R) {
-            //mid = (L + R)/2;//直接加会越界
-            mid = L + ((R - L) >> 1);//L + (R - L)/2
+            // 直接加除会溢出：mid = (L + R)/2;
+            // 优化除法：     L + (R - L)/2
+            mid = L + ((R - L) >> 1);
+
+            // L...mid...R
+            // L..R
+            //        L..R
             if (sortedArr[mid] == num) {
+                // 直接找到
                 return true;
             } else if (sortedArr[mid] > num) {
                 R = mid - 1;
@@ -26,12 +35,13 @@ public class Code01_BinarySearchExist {
             }
         }
 
+        // 如果while(L<R)，L～R至少2个数，L==R的时候停止循环，需要单独判断最后一个数是否是num
+        // return sortedArr[L] == num;
         return false;
-        //sortedArr[L] == num;
-        //如果while(L<R)，L==R的时候停止循环，需要单独判断L位置的
     }
 
     public static void main(String[] args) {
-        //System.out.println(binarySearchExist(new int[]{1, 2, 3, 4, 5, 6},0));
+        System.out.println(binarySearchExist(new int[]{1, 2, 3, 4, 5, 6}, 0));
     }
+
 }
--- DataStructureAndAlgorithm/src/main/java/P03_BinarySearch/Code04_BinarySearchMinimum.java
+++ DataStructureAndAlgorithm/src/main/java/P03_BinarySearch/Code04_BinarySearchMinimum.java
@@ -1,23 +1,32 @@
 package P03_BinarySearch;
 
 /**
- * @author : ZWH 2021/5/25
- * @version : 1.0
+ * @author : ZWH
+ * @date : 2021/05/25
+ * @Description : 二分法：局部最小值问题=找极值
  */
 public class Code04_BinarySearchMinimum {
-    public int firstMinimumIndex (int[] arr) {
-        if (arr == null || arr.length == 0) return -1;//no exist
 
-        if (arr.length == 1 || arr[0] < arr[1])
+    public int firstFindMinimumIndex(int[] arr) {
+        // 无元素
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        // 边界
+        if (arr.length == 1 || arr[0] < arr[1]) {
             return 0;
-        if (arr[arr.length - 1] <arr[arr.length - 2])
+        }
+        if (arr[arr.length - 1] < arr[arr.length - 2]) {
             return arr.length - 1;
+        }
 
         int L = 1;
         int R = arr.length - 2;
         int mid;
+        // 停止条件：L～R至少1个数
         while (L <= R) {
             mid = L + ((R - L) >> 1);
+
             if (arr[mid] > arr[mid - 1]) {
                 R = mid - 1;
             } else if (arr[mid] > arr[mid + 1]) {
@@ -26,6 +35,10 @@ public class Code04_BinarySearchMinimum {
                 return mid;
             }
         }
+
+        // 如果while(L<R)，L～R至少2个数，L==R的时候停止循环，最后一个数就是局部最小
+        // return L;
         return -1;
     }
+
 }
