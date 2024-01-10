1c62962a472cb4cfe771e11f11e2a16d9b0f65d8
zhengweihong <529729644@qq.com>
2024-01-10 13:34:45 +0800

    设计模式 门面模式

---
+++ DesignPattern/src/main/java/P04_Facade_Mediator/GameViewFacade.java
@@ -0,0 +1,12 @@
+package P04_Facade_Mediator;
+
+/**
+ * @author : ZWH
+ * @date : 2024/01/10
+ * @Description : 门面模式
+ */
+public interface GameViewFacade {
+    void showVehicle();
+
+    void showWeapon();
+}
---
+++ DesignPattern/src/main/java/P04_Facade_Mediator/GameViewFacadeImpl.java
@@ -0,0 +1,18 @@
+package P04_Facade_Mediator;
+
+/**
+ * @author : ZWH
+ * @date : 2024/01/10
+ * @Description : 实现类
+ */
+public class GameViewFacadeImpl implements GameViewFacade {
+    @Override
+    public void showVehicle() {
+        // use service interfaces
+    }
+
+    @Override
+    public void showWeapon() {
+        // use service interfaces
+    }
+}
