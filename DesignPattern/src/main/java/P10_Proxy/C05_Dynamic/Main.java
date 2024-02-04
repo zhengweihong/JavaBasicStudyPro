aa32679c5c12df370c5b7c5e5c0e82956dad3da8
zhengweihong <529729644@qq.com>
2024-02-05 00:39:45 +0800

    设计模式 代理模式

--- DesignPattern/src/main/java/P10_Proxy/C05_Dynamic/Main.java
+++ DesignPattern/src/main/java/P10_Proxy/C05_Dynamic/Main.java
@@ -5,12 +5,13 @@ import java.lang.reflect.*;
 /**
  * @author : ZWH
  * @date : 2024/02/03
- * @Description : 代理模式：测试，代理任意类型的类，分离被代理对象和代理行为，JDK动态代理，运行时生成
+ * @Description : 代理模式：测试，代理任意类型的类，分离被代理对象和代理行为，JDK动态代理，运行时在内存生成
  */
 public class Main {
     public static void main(String[] args) {
         Car car = new Car();
 
+        // 把动态生成的代理类持久化
         System.getProperties().put("jdk.proxy.ProxyGenerator.saveGeneratedFiles", "true");
 
         // 动态生成代理类
@@ -22,6 +23,7 @@ public class Main {
                 Car.class.getInterfaces(),
                 // 传入函数：被代理对象的方法被调用时，代理都做什么处理
                 // 调用处理器类：InvocationHandler.invoke()
+                // 入参：生成的代理类对象、被代理的方法、被代理的方法入参
                 (proxy, method, methodArgs) -> {
                     long start = System.currentTimeMillis();
                     Object o = method.invoke(car, methodArgs);
