481d5292ceab17a87308a1c562a8ff6912af4ce0
zhengweihong <529729644@qq.com>
2024-04-12 14:38:06 +0800

    多线程 Thread类基本方法/常用API

--- JavaBasicGrammar/src/main/java/P01_Language/P04_Multithread/P01_Thread/Code02_ThreadApi.java
+++ JavaBasicGrammar/src/main/java/P01_Language/P04_Multithread/P01_Thread/Code02_ThreadApi.java
@@ -1,5 +1,7 @@
 package P01_Language.P04_Multithread.P01_Thread;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author : ZWH
  * @date : 2024/04/09
@@ -7,7 +9,7 @@ package P01_Language.P04_Multithread.P01_Thread;
  */
 public class Code02_ThreadApi {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // 当前主线程对象
         Thread thread = Thread.currentThread();
 
@@ -26,6 +28,12 @@ public class Code02_ThreadApi {
         System.out.println(thread.getPriority());
         thread.setPriority(10);
         System.out.println(thread.getPriority());
+
+        // 线程睡眠
+        // 同抛出InterruptedException
+        // 时间单位不同：上边毫秒需要换算、下边枚举类单位可选
+        Thread.sleep(1);
+        TimeUnit.SECONDS.sleep(1);
     }
 
 }
